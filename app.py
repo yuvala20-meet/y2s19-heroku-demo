3502f3432bb0357b83fc7b10f796c38b29de8511
myname21-meet <myname21@meet.mit.edu>
2019-07-31 13:18:29 +0300

    fixing

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_temlete
+from flask import Flask, render_template
 app = Flask(__name__)
 
+
+@app.route('/')
+def fashion():
+	return render_template('fasion.html')
+
 @app.route('/accessories')
-	def accessories():
-		return render_temlete("accessories.html")
+def accessories():
+	return render_template("accessories.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
